42456637f5483426ddfb20c6c94c73fed0d1a377
Joao Victor Assis <joaovictorassis03@gmail.com>
2025-10-19 19:19:11 -0300

    feat: Implementação de toda documentação utilizando Swagger

---
+++ src/docs/index.ts
@@ -0,0 +1,7 @@
+// Arquivo de agregação de todas as documentações
+// Este arquivo é importado pela configuração do Swagger
+
+// Importar todas as documentações
+import './auth';
+import './investimentos';
+import './health';
